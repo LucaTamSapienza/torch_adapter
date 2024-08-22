11073e5ad9fb1b9f75f8c9b6cad938a0e7a255a9
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2024-08-22 19:28:07 +0200

    replaced absolute path with the relative ones

--- src/torch_adapter/hub/__init__.py
+++ src/torch_adapter/hub/__init__.py
@@ -1,5 +1,5 @@
 import torch.hub 
-from src.torch_adapter.util import AdapterModel
+from ..util import AdapterModel
 
 def load(*args, **kwargs):
     return AdapterModel(torch.hub.load(*args, **kwargs))
--- src/torch_adapter/transforms/__init__.py
+++ src/torch_adapter/transforms/__init__.py
@@ -1,3 +1,3 @@
-from src.torch_adapter.transforms.transform_op import *
-from src.torch_adapter.transforms.composition import *
+from .transform_op import *
+from .composition import *
 
